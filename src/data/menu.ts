930dc4815a2f3f7caeac1e80d60719267460271d
Aniket Patel <aniketpatel.0303@gmail.com>
2025-07-11 06:02:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -5,54 +5,56 @@ export interface MenuItem {
   image?: string;
 }
 
+const newImage = "https://images.unsplash.com/photo-1664717698774-84f62382613b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxhLWJvd2wtb2YtcmljZS13aXRoLXZlZ2V0YWJsZXMtTVZNb2hKQmllbzR8ZW58MHx8fHwxNzUyMjEyNzczfDA&ixlib=rb-4.1.0&q=80&w=1080";
+
 export const menu: Record<string, MenuItem[]> = {
   "SOUP": [
-    { name: "Manchau Soup", half: 70, image: "https://placehold.co/100x100.png" },
-    { name: "Hot & Sour Soup", half: 70, image: "https://placehold.co/100x100.png" },
-    { name: "Tomato Soup", half: 70, image: "https://placehold.co/100x100.png" },
-    { name: "Paneer Soup", full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Manchurian Soup", half: 70, image: "https://placehold.co/100x100.png" },
-    { name: "Noodle's Soup", half: 70, image: "https://placehold.co/100x100.png" },
-    { name: "Babycorn Soup", full: 120, image: "https://placehold.co/100x100.png" }
+    { name: "Manchau Soup", half: 70, image: newImage },
+    { name: "Hot & Sour Soup", half: 70, image: newImage },
+    { name: "Tomato Soup", half: 70, image: newImage },
+    { name: "Paneer Soup", full: 100, image: newImage },
+    { name: "Manchurian Soup", half: 70, image: newImage },
+    { name: "Noodle's Soup", half: 70, image: newImage },
+    { name: "Babycorn Soup", full: 120, image: newImage }
   ],
   "NOODLES": [
-    { name: "Veg. Hongkong Noodles", full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Hakka Noodles", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Noodle Manchurian", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Schezwan Noodles", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Singapuri Noodles", full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Paneer Noodles", full: 150, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Rice Noodles", full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Gravy Noodles", full: 100, image: "https://placehold.co/100x100.png" }
+    { name: "Veg. Hongkong Noodles", full: 100, image: newImage },
+    { name: "Veg. Hakka Noodles", half: 70, full: 100, image: newImage },
+    { name: "Veg. Noodle Manchurian", half: 70, full: 100, image: newImage },
+    { name: "Veg. Schezwan Noodles", half: 70, full: 100, image: newImage },
+    { name: "Veg. Singapuri Noodles", full: 100, image: newImage },
+    { name: "Veg. Paneer Noodles", full: 150, image: newImage },
+    { name: "Veg. Rice Noodles", full: 100, image: newImage },
+    { name: "Veg. Gravy Noodles", full: 100, image: newImage }
   ],
   "BHEL": [
-    { name: "Chinese Bhel", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Bombay Bhel", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Gravy Bhel", full: 100, image: "https://placehold.co/100x100.png" }
+    { name: "Chinese Bhel", half: 70, full: 100, image: newImage },
+    { name: "Bombay Bhel", half: 70, full: 100, image: newImage },
+    { name: "Gravy Bhel", full: 100, image: newImage }
   ],
   "MANCHURIAN": [
-    { name: "Veg. Manchurian Dry", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Manchurian Gravy", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Manchurian 65", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Schezwan Manchurian Dry", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Schezwan Manchurian Gravy", half: 70, full: 100, image: "https://placehold.co/100x100.png" }
+    { name: "Veg. Manchurian Dry", half: 70, full: 100, image: newImage },
+    { name: "Veg. Manchurian Gravy", half: 70, full: 100, image: newImage },
+    { name: "Veg. Manchurian 65", half: 70, full: 100, image: newImage },
+    { name: "Schezwan Manchurian Dry", half: 70, full: 100, image: newImage },
+    { name: "Schezwan Manchurian Gravy", half: 70, full: 100, image: newImage }
   ],
   "PANEER": [
-    { name: "Paneer Chilli Dry", half: 90, full: 140, image: "https://placehold.co/100x100.png" },
-    { name: "Paneer Chilli Gravy", half: 90, full: 140, image: "https://placehold.co/100x100.png" },
-    { name: "Paneer Chilli 65", full: 140, image: "https://placehold.co/100x100.png" },
-    { name: "Paneer Manchurian Dry", full: 150, image: "https://placehold.co/100x100.png" },
-    { name: "Paneer Manchurian Gravy", full: 150, image: "https://placehold.co/100x100.png" }
+    { name: "Paneer Chilli Dry", half: 90, full: 140, image: newImage },
+    { name: "Paneer Chilli Gravy", half: 90, full: 140, image: newImage },
+    { name: "Paneer Chilli 65", full: 140, image: newImage },
+    { name: "Paneer Manchurian Dry", full: 150, image: newImage },
+    { name: "Paneer Manchurian Gravy", full: 150, image: newImage }
   ],
   "RICE": [
-    { name: "Veg. Rice", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Hong Kong Rice", full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Machurian Rice", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Singapuri Rice", full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Paneer Rice", full: 140, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Triple Rice", full: 150, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Schezwan Rice", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Veg. Combination Rice", half: 70, full: 100, image: "https://placehold.co/100x100.png" },
-    { name: "Babycorn Rice", full: 120, image: "https://placehold.co/100x100.png" }
+    { name: "Veg. Rice", half: 70, full: 100, image: newImage },
+    { name: "Veg. Hong Kong Rice", full: 100, image: newImage },
+    { name: "Veg. Machurian Rice", half: 70, full: 100, image: newImage },
+    { name: "Veg. Singapuri Rice", full: 100, image: newImage },
+    { name: "Veg. Paneer Rice", full: 140, image: newImage },
+    { name: "Veg. Triple Rice", full: 150, image: newImage },
+    { name: "Veg. Schezwan Rice", half: 70, full: 100, image: newImage },
+    { name: "Veg. Combination Rice", half: 70, full: 100, image: newImage },
+    { name: "Babycorn Rice", full: 120, image: newImage }
   ]
 };
