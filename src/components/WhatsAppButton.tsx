52e8b4a99269befcf067a9495f42d351480214ab
8BitAK <aniketpatel.0303@gmail.com>
2025-11-27 13:47:12 +0530

    Update WhatsAppButton.tsx
    whtassapp updated for rahul patel number

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 export function WhatsAppButton() {
   return (
     <a 
-      href="https://wa.me/919898701230" 
+      href="https://wa.me/918780063939" 
       target="_blank" 
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 group"
